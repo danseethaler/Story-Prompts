a6b9988836fbecaddd0773857fa3f2f4537c7506
Dan Seethaler <dan@seethalers.com>
2020-11-07 13:32:04 -0700

    Add React Navigation to the project

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import {StatusBar} from 'react-native';
 import {ThemeProvider, themes} from 'wStyled';
@@ -5,10 +7,12 @@ import Dashboard from './src/Dashboard';
 
 const App: React.FC = () => {
   return (
-    <ThemeProvider theme={themes.light}>
-      <Dashboard />
-      <StatusBar barStyle="light-content" />
-    </ThemeProvider>
+    <NavigationContainer>
+      <ThemeProvider theme={themes.light}>
+        <Dashboard />
+        <StatusBar barStyle="light-content" />
+      </ThemeProvider>
+    </NavigationContainer>
   );
 };
 
