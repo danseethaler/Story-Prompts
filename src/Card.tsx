d8f02a8c70be4a529f9efcacadc85c212aa38a4e
Dan Seethaler <dan@seethalers.com>
2020-11-07 11:34:09 -0700

    Add transform animations for cards

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,100 +1,110 @@
 import {FontAwesome, Ionicons} from '@expo/vector-icons';
 import React from 'react';
-import {Text} from 'react-native';
+import {Animated, Text} from 'react-native';
 import WContainer from './components/WContainer';
 import {categories} from './config/cardData';
 import {CardType} from './definitions';
 import {useStyledTheme} from './styled/styled';
 import {hexToRgb} from './styled/utils';
 
-interface Props extends CardType {}
+interface Props extends CardType {
+  transforms: any;
+}
 
-const Card: React.FC<Props> = ({quote, prompt, category, song = false}) => {
+const Card: React.FC<Props> = ({
+  quote,
+  prompt,
+  category,
+  transforms,
+  song = false,
+}) => {
   const theme = useStyledTheme();
   const categoryColor = theme.categoryColors[category];
   const {title} = categories[category];
 
   return (
     <WContainer justify="space-between" align="center" stretch>
-      <WContainer
-        align="center"
-        flex={1}
-        wPadding={[6, 3]}
-        justify="space-between"
-        stretch>
+      <Animated.View style={{transform: transforms}}>
         <WContainer
-          wPadding={[0.5, 2]}
-          style={{
-            backgroundColor: theme.categoryColorsLight[category],
-            borderRadius: 22,
-          }}>
-          <Text
+          align="center"
+          flex={1}
+          wPadding={[6, 3]}
+          justify="space-between"
+          stretch>
+          <WContainer
+            wPadding={[0.5, 2]}
             style={{
-              fontWeight: '600',
-              letterSpacing: 1.4,
-              fontFamily: 'Avenir Next',
-              textAlign: 'center',
-              fontSize: 18,
-              color: '#1B1B24',
+              backgroundColor: theme.categoryColorsLight[category],
+              borderRadius: 22,
             }}>
-            {title}
-          </Text>
-        </WContainer>
-
-        <WContainer stretch row wPadding={[3, 0]}>
-          {song ? (
-            <Ionicons
-              name="ios-musical-note"
-              size={20}
-              style={{alignSelf: 'flex-start'}}
-              color={theme.colors.text100}
-            />
-          ) : (
-            <FontAwesome
-              name="quote-left"
-              size={20}
-              style={{alignSelf: 'flex-start'}}
-              color={theme.colors.text100}
-            />
-          )}
-          <WContainer align="center" flex={1} wPadding={2}>
             <Text
               style={{
+                fontWeight: '600',
+                letterSpacing: 1.4,
+                fontFamily: 'Avenir Next',
                 textAlign: 'center',
-                fontSize: 32,
-                color: theme.colors.white,
+                fontSize: 18,
+                color: '#1B1B24',
               }}>
-              {quote}
+              {title}
             </Text>
           </WContainer>
-          {song ? (
-            <Ionicons
-              name="ios-musical-notes"
-              size={20}
-              style={{alignSelf: 'flex-end'}}
-              color={theme.colors.text100}
-            />
-          ) : (
-            <FontAwesome
-              name="quote-right"
-              size={20}
-              style={{alignSelf: 'flex-end'}}
-              color={theme.colors.text100}
-            />
-          )}
-        </WContainer>
 
-        <Text
-          style={{
-            textAlign: 'center',
-            fontSize: 22,
-            fontFamily: 'Avenir Next',
-            fontWeight: '500',
-            color: hexToRgb(categoryColor, 1, 3.5),
-          }}>
-          {prompt}
-        </Text>
-      </WContainer>
+          <WContainer stretch row wPadding={[3, 0]}>
+            {song ? (
+              <Ionicons
+                name="ios-musical-note"
+                size={20}
+                style={{alignSelf: 'flex-start'}}
+                color={theme.colors.text100}
+              />
+            ) : (
+              <FontAwesome
+                name="quote-left"
+                size={20}
+                style={{alignSelf: 'flex-start'}}
+                color={theme.colors.text100}
+              />
+            )}
+            <WContainer align="center" flex={1} wPadding={2}>
+              <Text
+                style={{
+                  textAlign: 'center',
+                  fontSize: 32,
+                  color: theme.colors.white,
+                }}>
+                {quote}
+              </Text>
+            </WContainer>
+            {song ? (
+              <Ionicons
+                name="ios-musical-notes"
+                size={20}
+                style={{alignSelf: 'flex-end'}}
+                color={theme.colors.text100}
+              />
+            ) : (
+              <FontAwesome
+                name="quote-right"
+                size={20}
+                style={{alignSelf: 'flex-end'}}
+                color={theme.colors.text100}
+              />
+            )}
+          </WContainer>
+
+          <Text
+            style={{
+              textAlign: 'center',
+              fontSize: 22,
+              fontFamily: 'Avenir Next',
+              fontWeight: '500',
+              color: hexToRgb(categoryColor, 1, 3.5),
+            }}>
+            {prompt}
+          </Text>
+        </WContainer>
+      </Animated.View>
     </WContainer>
   );
 };
--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,6 +1,7 @@
 import ViewPager from '@react-native-community/viewpager';
 import _ from 'lodash';
 import React, {useRef, useState} from 'react';
+import {Animated} from 'react-native';
 import {BothSafeArea, NextButton} from 'wComponents';
 import {useGetColorsFromCards} from 'wHooks';
 import Card from './Card';
@@ -18,9 +19,14 @@ const Dashboard: React.FC = () => {
     shuffledCards,
   );
   const colors = getColorsFromCards(cardIndex);
-  console.log('colorsasdfasf', colors);
 
   const viewPagerRef = useRef<ViewPager>();
+
+  const animatedPositionValue = useRef(new Animated.Value(0)).current;
+  const scrollPositionUpdater = Animated.event(
+    [{offset: animatedPositionValue}],
+    {useNativeDriver: false},
+  );
 
   return (
     <BothSafeArea bottomColor="#1B1B24">
@@ -31,13 +37,34 @@ const Dashboard: React.FC = () => {
           initialPage={0}
           onPageScroll={(event) => {
             scrollUpdater(event);
+
+            const fullOffset =
+              event.nativeEvent.position + event.nativeEvent.offset;
+            scrollPositionUpdater({offset: fullOffset});
+
             if (cardIndex !== event.nativeEvent.position) {
               setCardIndex(event.nativeEvent.position);
             }
           }}>
-          {_.map(shuffledCards, (card) => (
-            <Card key={card.quote} {...card} />
-          ))}
+          {_.map(shuffledCards, (card, cardIndex) => {
+            const positionTransform = animatedPositionValue.interpolate({
+              inputRange: [cardIndex - 1, cardIndex, cardIndex + 1],
+              outputRange: [50, 0, 50],
+              extrapolate: 'clamp',
+            });
+            const scaleTransform = animatedPositionValue.interpolate({
+              inputRange: [cardIndex - 1, cardIndex, cardIndex + 1],
+              outputRange: [0.9, 1, 0.9],
+              extrapolate: 'clamp',
+            });
+
+            const transforms = [
+              {translateY: positionTransform},
+              {scale: scaleTransform},
+            ];
+
+            return <Card key={card.quote} transforms={transforms} {...card} />;
+          })}
         </ViewPager>
         <WContainer align="center" stretch>
           <NextButton
