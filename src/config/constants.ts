99bd44f84adfc7293e8c122d9e1ff486f55be143
Dan Seethaler <dan@seethalers.com>
2020-12-08 21:07:23 -0700

    Update App Store URL

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,3 @@
 export const CARD_DRAG_RANGE = 250;
 export const APP_STORE_URL =
-  'https://www.kickstarter.com/projects/1449215381/stories-1';
+  'https://apps.apple.com/us/app/story-packs/id1544016167';
